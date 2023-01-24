81c73bc1246dbb1b961cccd5afe24c011e19f93b
nguyen <nguyen@digital-launch.com>
2023-01-24 17:30:50 +0700

    use Express

--- jwt/code/src/server.js
+++ jwt/code/src/server.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json())
+
+app.get('/books', (req, res) => {
+    res.json({status: 'Success! Get paid by your work!'})
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 })

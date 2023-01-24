a49bee7b2f63322410c883753fcaffee65b096b5
nguyen <nguyen@digital-launch.com>
2023-01-24 17:37:44 +0700

    use Express 2

--- jwt/code/src/server.js
+++ jwt/code/src/server.js
@@ -5,8 +5,29 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 
+const books = [
+    {
+        id: 1,
+        name: 'ReactJS',
+        author: 'A'
+    },
+    {
+        id: 2,
+        name: 'JWT',
+        author: 'B'
+    },
+    {
+        id: 3,
+        name: 'Ky nang tu hoc',
+        author: 'Nguyen'
+    }
+]
+
 app.get('/books', (req, res) => {
-    res.json({status: 'Success! Get paid by your work!'})
+    res.json({
+        status: 'Success! Get paid by your work! Demand on JS skill!',
+        data: books
+    })
 })
 
 app.listen(PORT, () => {
